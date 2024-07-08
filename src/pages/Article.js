dd49720a2a47bce496d44f0e05e0b03f0b463800
Anjana Kavindu <anjanakavinducampus@gmail.com>
2024-07-08 14:32:28 +0530

    URL parameter with react router useparams

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
 
 const Article = () => {
+
+    const { name } = useParams();
+    const article = articleContent.find((article) => article.name === name);
+    if(!article) {
+        return <h1>Article does not exist</h1>
+    }
+
     return (
       <div>
-        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>This is Article</h1>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+            {article.title}
+        </h1>
+          {article.content.map((paragraph) => (
+              <p className="mx-auto leading-relaxed text-base mb-4">
+                  {paragraph}
+              </p>
+          ))}
       </div>
     );
 }
